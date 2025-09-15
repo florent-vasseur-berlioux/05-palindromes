1e64bdefaf332e04b9617ef9e303f99763165c9a
FlorentVASSEURBERLIOUX <vasseurberlioux.florent@gmail.com>
2025-09-15 15:01:32 +0200

    Rendu du travail sur les palindromes

---
+++ main.py
@@ -0,0 +1,22 @@
+#### Fonction secondaire
+
+
+def ispalindrome(p):
+
+    if p == p[::-1]:
+        return True
+    return False
+
+#### Fonction principale
+
+
+def main():
+
+    # vos appels à la fonction secondaire ici
+
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+        print(s, ispalindrome(s))
+
+
+if __name__ == "__main__":
+    main()
